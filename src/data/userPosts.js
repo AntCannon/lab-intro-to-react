bea618741777a96df0de24b8892cd092838266ee
AntCannon <anthonycannonier@gmail.com>
2024-03-28 16:34:39 -0400

    create userPosts data

---
+++ src/data/userPosts.js
@@ -0,0 +1,20 @@
+const userPosts =[
+  {
+    title: "Post Num 1",
+    post: "This is post num 1"
+  },
+  {
+    title: "Post Num 2",
+    post: "This is post num 2"
+  },
+  {
+    title: "Post Num 3",
+    post: "This is post num 3"
+  },
+  {
+    title: "Post Num 4",
+    post: "This is post num 4"
+  },
+]
+
+export default userPosts;
