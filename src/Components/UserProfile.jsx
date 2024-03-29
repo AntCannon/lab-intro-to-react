8971b8542cf1f2484367870aa13001b2e2f0934b
AntCannon <anthonycannonier@gmail.com>
2024-03-28 23:18:07 -0400

    update UserProfile elements

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -3,13 +3,15 @@ import profileImg from "../profile.png";
 export default function UserProfile({user}) {
   
   return (
-    <div className="user-card card">
-      <img className="user-avatar" src={profileImg} />
-      <div className="user-details">
-        <h2>{user.firstName} {user.lastName}</h2>
-        <h3>{user.position}</h3>
-        <span>{user.headline}</span>
+    <section className="user">
+      <div className="user-card card">
+        <img className="user-avatar" src={profileImg} />
+        <div className="user-details">
+          <h2>{user.firstName} {user.lastName}</h2>
+          <h3>{user.position}</h3>
+          <span>{user.headline}</span>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
