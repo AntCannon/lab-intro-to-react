a00c08610b0339a0a9fb11c88227d0ee2a8d3114
AntCannon <anthonycannonier@gmail.com>
2024-03-28 22:20:26 -0400

    add postId keys

--- src/data/userPosts.js
+++ src/data/userPosts.js
@@ -1,19 +1,23 @@
 const userPosts =[
   {
     title: "Post Num 1",
-    post: "This is post num 1"
+    post: "This is post num 1",
+    postId: 198404
   },
   {
     title: "Post Num 2",
-    post: "This is post num 2"
+    post: "This is post num 2",
+    postId: 387937
   },
   {
     title: "Post Num 3",
-    post: "This is post num 3"
+    post: "This is post num 3",
+    postId: 7294793
   },
   {
     title: "Post Num 4",
-    post: "This is post num 4"
+    post: "This is post num 4",
+    postId: 4088035
   },
 ]
 
